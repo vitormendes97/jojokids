2cd3863c2963a46623246b8d75e50b03bb6f0f35
Vitor Mendes <vitor.mendes@salaovip.com.br>
2017-02-09 18:56:19 -0200

    Ajustes finais

--- js/custom/svip.js
+++ js/custom/svip.js
@@ -4,5 +4,8 @@ jQuery(document).ready(function() {
 	// 	jQuery('#video-space').attr('data-columns' , '1');
 	// }
 
-
+    if(jQuery(window).width() < 790) {
+        jQuery('.element1').attr('data-voffset' , '-290');
+        jQuery('.element2').removeClass('.mainslider_1 li:nth-child(1) .tp-caption._bs_slider_title + ._bs_slider_title');
+    }
 });
